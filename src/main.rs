aca3053283ff0f62bd82300e86d5f0c4859ae605
Daniel Rush <dmrush529@gmail.com>
2025-10-20 13:31:18 -0600

    Added comments

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,24 @@ use std::io::{stdout, Write};
 use crossterm::queue;
 
 fn main() {
-    //basic test stuff to start, try reading the component temps on my device
+    //create new sysinfo entry
     let mut sys = sysinfo::System::new_all();
 
-    //try looping this bit, expect that it will print new set per lines
+    //loop to constantly display temp data
     loop {
+        //create stdout
         let mut stdout = stdout();
+        //clear all lines printed by the last print
         queue!(stdout, Clear(ClearType::All)).unwrap();
         stdout.flush().unwrap();
-        //iterate each component individually now
+        //iterate each component and refresh their info
         sys.refresh_specifics(RefreshKind::nothing());
         let components = Components::new_with_refreshed_list();
         //print to terminal
         for component in &components {
             println!("{component:?}");
         };
+        //wait one second before doing it again
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
