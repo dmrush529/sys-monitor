c9beff7948a88684cb8bd2e0300fe6e246034d83
Daniel Rush <dmrush529@gmail.com>
2025-10-25 19:12:52 -0600

    Reworked loop and clearing functionality, added feature, removed old dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
 //system monitor specifically for component temps
 //import sysinfo crate
-use sysinfo::{Components, RefreshKind};
-use crossterm::terminal::{Clear, ClearType};
-use std::io::{stdout, Write};
-use crossterm::queue;
+use sysinfo::*;
 
 fn main() {
-    //create new sysinfo entry
-    let mut sys = sysinfo::System::new_all();
+    //create new system entry with sysinfo
+    let mut sys = System::new_all();
 
     //loop to constantly display temp data
     loop {
-        //create stdout
-        let mut stdout = stdout();
         //clear all lines printed by the last print
-        queue!(stdout, Clear(ClearType::All)).unwrap();
-        stdout.flush().unwrap();
+        println!("{esc}c", esc = 27 as char);
         //iterate each component and refresh their info
-        sys.refresh_specifics(RefreshKind::nothing());
+        sys.refresh_all();
         let components = Components::new_with_refreshed_list();
         //print to terminal
         for component in &components {
@@ -26,4 +20,5 @@ fn main() {
         //wait one second before doing it again
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
+
 }
